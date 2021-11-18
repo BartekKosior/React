6a449b17cc03bbfa0175cf97240e6aa3e5e3bc8c
Bartek Kosior <bartlomiejkosior2@gmail.com>
2021-11-18 20:33:06 +0100

    Add components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import styles from './App.scss';
+import List from '../List/List.js';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      /*<div>
         <h1>My first React app</h1>
-      </div>
+        <h2> Hello world !</h2>
+      </div>*/
+
+      <main className = {styles.component}>
+        <h1 className = {styles.title}> My first React app </h1>
+        <h2 className = {styles.subtitle}> Hello World ! </h2>  
+        <List title = {['Thinks to do', <sup key='1'> soon! </sup>]}>
+          
+        </List>
+        
+        
+      </main>
+
+    
     )
   }
 }
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Hero.scss';
+import PropTypes from 'prop-types';
+
+class Hero extends React.Component{
+    render(){
+        return (
+            <header className = {styles.component}>
+                <h2 className = {styles.title}> {this.props.titleText} </h2>
+                <img className = {styles.image} src = "http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" />
+            </header>
+        )
+    }
+}
+
+Hero.propTypes = {
+    titleText: PropTypes.node,
+};
+
+export default Hero
